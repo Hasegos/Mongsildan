6d712a9faa8b75544e090f834a7bb9b3b96fe603
Hasegos <csw020106@naver.com>
2025-04-22 16:12:58 +0900

    2025-04-22 수정본

--- home/home.js
+++ home/home.js
@@ -26,4 +26,22 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch((err) => {
       console.error("fetch 실패:", err);
     });
+
+  fetch("../aside/aside.html")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("파일을 불러오지 못했습니다.");
+      }
+      return res.text();
+    })
+    .then((html) => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+
+      const bodyContent = doc.body.innerHTML;
+      document.querySelector("aside").innerHTML = bodyContent;
+    })
+    .catch((err) => {
+      console.error("fetch 실패:", err);
+    });
 });
