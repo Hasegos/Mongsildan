4e701dd9da8c20d0699a59401d450455d93ddcc4
Hasegos <csw020106@naver.com>
2025-04-23 13:49:47 +0900

    home js 수정

--- project/home/home.js
+++ project/home/home.js
@@ -1,78 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("./상단바/header-sub.html")
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("파일을 불러오지 못했습니다.");
-      }
-      return res.text();
-    })
-    .then((html) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, "text/html");
 
-      const bodyContent = doc.body.innerHTML;
-      document.querySelector(".nav").innerHTML = bodyContent;
+   // 최상단바 불러오기
+  loadHtml("header", "./상단바/header-top.html", () => {
+    document
+      .getElementById("searchButton")
+      .addEventListener("click", function () {
+        const keyword = document.getElementById("searchInput").value.trim();
+        if (keyword) {
+          alert("검색어: " + keyword);
+        } else {
+          alert("검색어를 입력하세요!");
+        }
+      });     
+  });    
 
-      const scrollContainer = document.getElementById("scrollContainer");
-      const scrollBtn = document.getElementById("scrollRightBtn");
+  // 헤더 서브바 불러오기
+  loadHtml(".nav", "./상단바/header-sub.html", () => {
+    const scrollContainer = document.getElementById("scrollContainer");
+    const scrollBtn = document.getElementById("scrollRightBtn");
 
-      scrollBtn.addEventListener("click", () => {
-        scrollContainer.scrollBy({
-          left: 200,
-          behavior: "smooth",
+    scrollBtn.addEventListener("click", () => {      
+      scrollContainer.scrollBy({
+        left: 200,
+        behavior: "smooth",
+      });      
+    });
+  });
+
+  // 사이드바 불러오기
+  loadHtml("aside", "./sidebar/aside.html", () => {
+    const menuButton = document.getElementById("menuButton");
+    // width 넓이 수정을위해서
+    const aside = document.querySelector("aside");
+    let cuurrentPage = 1;
+    let styleLink = document.createElement("link");
+    styleLink.rel = "stylesheet";
+
+    menuButton.addEventListener("click", () => {
+      if(cuurrentPage == 1){        
+        loadHtml("aside","./sidebar/aside2.html", () => {            
+          styleLink.href = "./sidebar/style/aside2.css";
+          document.head.appendChild(styleLink);         
+              
+          aside.style.width = "70px";        
+          aside.style.display = "flex";
+          aside.style.justifyContent = "center";          
+          
         });
-      });
-    })
-    .catch((err) => {
-      console.error("fetch 실패:", err);
+        cuurrentPage = 2;
+      }      
+      else if (cuurrentPage == 2){
+        loadHtml("aside","./sidebar/aside.html",() =>{          
+          styleLink.href = "./sidebar/style/aside.css";
+          document.head.appendChild(styleLink); 
+          cuurrentPage = 1;     
+          aside.style.width = "240px";
+          aside.style.display = "";
+          aside.style.justifyContent = "";          
+        });        
+      }      
     });
-
-  fetch("./사이드바/aside.html")
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("파일을 불러오지 못했습니다.");
-      }
-      return res.text();
-    })
-    .then((html) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, "text/html");
-
-      const bodyContent = doc.body.innerHTML;
-      document.querySelector("aside").innerHTML = bodyContent;
-    })
-    .catch((err) => {
-      console.error("fetch 실패:", err);
-    });
-
-  fetch("./상단바/header-top.html")
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("파일을 불러오지 못했습니다.");
-      }
-      return res.text();
-    })
-    .then((html) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, "text/html");
-
-      const bodyContent = doc.body.innerHTML;
-      document.querySelector("header").innerHTML = bodyContent;
-
-      document
-        .getElementById("searchButton")
-        .addEventListener("click", function () {
-          const keyword = document.getElementById("searchInput").value.trim();
-          if (keyword) {
-            alert("검색어: " + keyword);
-          } else {
-            alert("검색어를 입력하세요!");
-          }
-        });
-    })
-    .catch((err) => {
-      console.error("fetch 실패:", err);
-    });
+  });   
 
   // 카드 생성
   function creatDiv() {
@@ -95,4 +83,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const Div = document.querySelector("section");
     Div.appendChild(creatDiv());
   }
+
+  
+  let cuurrentSidebarPage = null;
+  function handleResponsiveSidebar() {
+    const meideaQuery = window.matchMedia("(max-width: 1315px)");
+    if(meideaQuery.matches && cuurrentSidebarPage !==2){      
+      switchSidebar(2);
+      cuurrentSidebarPage = 2;
+    }
+    else if(!meideaQuery.matches && cuurrentSidebarPage !==1){
+      switchSidebar(1);
+      cuurrentSidebarPage = 1;
+    }
+  }
+    
+  handleResponsiveSidebar();
+  
+
+  window.addEventListener("resize", handleResponsiveSidebar);
+  
+
 });
