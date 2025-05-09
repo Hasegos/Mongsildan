e585f2d562e92f696dc9d34440fde73a03e92094
Hasegos <csw020106@naver.com>
2025-05-09 11:27:58 +0900

    채널 비디오 목록  api 예외처리

--- api/getChannelVideoList.js
+++ api/getChannelVideoList.js
@@ -1,13 +1,25 @@
 // 채널 아이디에 대한 채널 리스트
 async function getChannelVideoList(channelID) {
-    const channel = await fetch(`http://techfree-oreumi-api.kro.kr:5000/video/getChannelVideoList?channel_id=${channelID}`, {
-        method: "GET",
-        headers: {
-        "Content-Type": "application/json",
-        }
-    });
-    const channelInfo = await channel.json();  
-    return channelInfo;
-}
+    try{
+        const channel = await fetch(`http://techfree-oreumi-api.kro.kr:5000/video/getChannelVideoList?channel_id=${channelID}`, {
+            method: "GET",
+            headers: {
+            "Content-Type": "application/json",
+            }
+        });
 
+        if(!channel.ok){
+            throw new Error(`HTTP 오류 ${channel.status}`);
+        }
+        const channelInfo = await channel.json();  
+        return channelInfo;
+    }
+    catch(error){        
+        if(!window.location.href.includes("error.html")){
+            alert("채널 목록 데이터를 불러오는데 실패했습니다. 새로고침해주세요.");
+            location.href = "../error/error.html";
+        }
+        return null;
+    }    
+}
 window.getChannelVideoList = getChannelVideoList;
