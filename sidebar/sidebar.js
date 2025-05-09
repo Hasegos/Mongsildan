ef21b6e58528094f6e3e5acd7888b08cdca4a353
Hasegos <csw020106@naver.com>
2025-05-09 11:29:57 +0900

    사이드바 예외처리

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -12,56 +12,71 @@ function menuButton(cuurrentPage,check) {
                 link.remove();
             }
         })
-        if(cuurrentPage == 1){             
-            loadHtml("aside","../sidebar/html/aside2.html", () => {            
-                styleLink.href = "../sidebar/style/aside2.css";
-                document.head.appendChild(styleLink);  
-                // 홈 페이지
-                if(check == 1){                    
-                    cuurrentPage = 2;
-                }            
-                // 비디오 페이지    
-                else if(check == 2){
-                    cuurrentPage = 1;
-                }
-                // 채널페이지
-                else if(check == 3){            
-                    cuurrentPage = 2;                    
-                }
-            });            
+        if(cuurrentPage == 1){    
+            // 접힌 사이드바 불러오기 
+            try {         
+                loadHtml("aside","../sidebar/html/aside2.html", () => {            
+                    styleLink.href = "../sidebar/style/aside2.css";
+                    document.head.appendChild(styleLink);  
+                    // 홈 페이지
+                    if(check == 1){                    
+                        cuurrentPage = 2;
+                    }            
+                    // 비디오 페이지    
+                    else if(check == 2){
+                        cuurrentPage = 1;
+                    }
+                    // 채널페이지
+                    else if(check == 3){            
+                        cuurrentPage = 2;                    
+                    }
+                });  
+            }          
+            catch(error) {                
+                alert("사이드바 불러오기 중 오류 발생");
+                location.reload();
+            }
         }      
         else if (cuurrentPage == 2){
-            loadHtml("aside","../sidebar/html/aside.html",() =>{          
-                styleLink.href = "../sidebar/style/aside.css";
-                document.head.appendChild(styleLink);                 
-                
-                aside.style.display = "";
-                cuurrentPage = 1; 
-                if(window.innerWidth <= 625) {
-                    if(check == 3 || check == 1){                        
-                        cuurrentPage = 3;
-                        aside.classList.toggle("mobile-open");                       
+            // 펼친 사이드바 불러오기
+            try{
+                loadHtml("aside","../sidebar/html/aside.html",() =>{          
+                    styleLink.href = "../sidebar/style/aside.css";
+                    document.head.appendChild(styleLink);                 
+                    
+                    aside.style.display = "";
+                    cuurrentPage = 1; 
+                    if(window.innerWidth <= 625) {
+                        if(check == 3 || check == 1){                        
+                            cuurrentPage = 3;
+                            aside.classList.toggle("mobile-open");                       
+                        }
+                    } 
+                    
+                    if(window.innerWidth > 625){
+                        aside.classList.remove("mobile-open");
                     }
-                } 
-                
-                if(window.innerWidth > 625){
-                    aside.classList.remove("mobile-open");
-                }
-                
+                    
 
-                if(check == 2) {
-                    aside.style.width = "0px";
-                    cuurrentPage = 3;
-                }
-                else {
-                    cuurrentPage = 1;      
-                }
-            });            
+                    if(check == 2) {
+                        aside.style.width = "0px";
+                        cuurrentPage = 3;
+                    }
+                    else {
+                        cuurrentPage = 1;      
+                    }
+                }); 
+            }           
+            catch(error){
+                alert("사이드바 불러오기 중 오류 발생");
+                location.reload();
+            }
         }  
         else if(cuurrentPage == 3){ 
             aside.style.display = "none";             
             cuurrentPage = 2;           
         }    
     });
+
 }
 window.menuButton = menuButton;
