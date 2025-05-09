fe7602c89b82ce9b86ccba7039da6412e38fb27a
Hasegos <csw020106@naver.com>
2025-05-09 11:28:21 +0900

    비디오 리스트 api 예외처리

--- api/getVideoList.js
+++ api/getVideoList.js
@@ -1,7 +1,21 @@
 // 총 영상 정보
 async function getVideoList() {
-    const channel = await fetch(`http://techfree-oreumi-api.kro.kr:5000/video/getVideoList`);       
-    const channelInfo = await channel.json();  
-    return channelInfo;
+    try{
+        const videoList = await fetch(`http://techfree-oreumi-api.kro.kr:5000/video/getVideoList`);  
+        
+        if(!videoList.ok){
+            throw new Error(`HTTP 오류 ${videoList.status}`);
+        }
+
+        const videoListInfo = await videoList.json();  
+        return videoListInfo;
+    }
+    catch(error){
+        if(!window.location.href.includes("error.html")){
+            alert("비디오 목록 데이터를 불러오는데 실패했습니다. 새로고침해주세요");
+            location.href = "../error/error.html";            
+        }        
+        return null;
+    }
 }
 window.getVideoList = getVideoList;
