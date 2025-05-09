b49c838ce57df232b32cccd8cc26aef0178935a0
Hasegos <csw020106@naver.com>
2025-05-09 11:28:10 +0900

    비디오 정보 api 예외처리

--- api/getVideoInfo.js
+++ api/getVideoInfo.js
@@ -1,13 +1,26 @@
 // 영상 정보
-async function getVideoInfo(channelID) {
-    const channel = await fetch(`http://techfree-oreumi-api.kro.kr:5000/video/getVideoInfo?video_id=${channelID}`, {
-        method: "GET",
-        headers: {
-        "Content-Type": "application/json",
-        }
-    });
-    const channelInfo = await channel.json();  
-    return channelInfo;
-}
+async function getVideoInfo(videoID) {
+    try{
+        const video = await fetch(`http://techfree-oreumi-api.kro.kr:5000/video/getVideoInfo?video_id=${videoID}`, {
+            method: "GET",
+            headers: {
+            "Content-Type": "application/json",
+            }
+        });
 
+        if(!video.status){
+            throw new Error(`HTTP 오류 ${video.status}`);
+        }
+
+        const videoInfo = await video.json();  
+        return videoInfo;
+    }
+    catch(error){        
+        if(!window.location.href.includes("error.html")){
+            alert("영상 정보를 불러오는데 실패했습니다. 새로고침해주세요.");   
+            location.href = "../error/error.html";      
+        }
+        return null;
+    }
+}
 window.getVideoInfo = getVideoInfo;
