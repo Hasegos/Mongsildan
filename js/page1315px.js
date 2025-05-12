1ae42d971047eac7f0c8baa0340cbc658e38cc20
Hasegos <csw020106@naver.com>
2025-05-12 19:33:18 +0900

    1315px 사이드바 초기 구독 상태 추가

--- js/page1315px.js
+++ js/page1315px.js
@@ -10,12 +10,16 @@ function switchSidebar(page) {
     if (page === 1) {
         loadHtml("aside", "../sidebar/html/aside.html", () => {
         styleLink.href = "../sidebar/style/aside.css";
+        // 초기 구독상태 확인
+        renderSavedSubscriptions(aside);
         document.head.appendChild(styleLink);      
     });
     }
     else if (page === 2) {
         loadHtml("aside", "../sidebar/html/aside2.html", () => {
         styleLink.href = "../sidebar/style/aside2.css";
+        // 초기 구독상태 확인
+        renderSavedSubscriptions(aside);
         document.head.appendChild(styleLink);        
         });
     }
