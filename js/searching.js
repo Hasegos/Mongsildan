2c357abd4070ed941c34ea74987a76cd1e58f2a9
Hasegos <csw020106@naver.com>
2025-05-15 17:56:53 +0900

    검색 페이지로 이동 주석 수정

--- js/searching.js
+++ js/searching.js
@@ -1,31 +1,26 @@
-/* 검색시 해당 페이지 확인 */
+/* =========== 검색시 해당 페이지 확인 =========== */
 function searching() {
     const searchButton = document.getElementById("searchButton");
     const searchInput = document.getElementById("searchInput");
 
-    function handleSearch(e){
-        
+    function handleSearch(e){        
         const raw = searchInput.value.trim();
-        const keyword = raw.replace(/\s/g, "");
-        
+        const keyword = raw.replace(/\s/g, "");        
         if(keyword){
             if(window.location.pathname.includes('home')) {
                 window.isSearching = true;
                 searchVideos(keyword);
             }
             else {
-                const url = `../../index.html?search=${encodeURIComponent(keyword)}`;
+                const url = `../index.html?search=${encodeURIComponent(keyword)}`;
                 window.location.href = url;             
             }
         }
         else{
             alert("검색어를 입력하세요!");
         }        
-    }
-
-    // 버튼 클릭시
-    searchButton.addEventListener("click", handleSearch);
-    // 엔터키 입력시
+    }    
+    searchButton.addEventListener("click", handleSearch);    
     searchInput.addEventListener("keydown", function (e) {
         if (e.key === "Enter" && !e.shiftKey) {                                
             handleSearch();
